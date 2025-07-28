4894fa9fc1fc6cfdd9d5aa007f28003552447ec5
KuzanovTecnologies <185839643+KuzanovTecnologies@users.noreply.github.com>
2025-07-28 19:44:52 -0300

    Update SolaximScanner.sh

--- SolaximScanner.sh
+++ SolaximScanner.sh
@@ -1,10 +1,26 @@
-#!/usr/bin/env bash 
-WhatWeb -- find [S1 & S2] == define potential Target IP Address: 68.87.115.135;
-DNSRecon -- find [S1 & S2] == if DNS IPv4: 68.87.115.135;
-BusterWeb -- find [S1 & S2] == if Upcoming Connections: Search for potential targets;
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
 
-if IPv4 -- find [S1 & S2] == 68.87.115.135;
-if WhatWeb -- find [S1 & S2] == 68.87.115.135;
-if DNSRecon -- find [S1 & S2] == 68.87.115.135;
+# Target IP
+TARGET_IP="68.87.115.135"
 
-if BusterWeb -- find [S1 & S2] == find any potential cyber threat agent IP Address;
+echo "[+] Running WhatWeb on TARGET_IP..."
+if command -v whatweb >/dev/null 2>&1; then
+    whatweb $TARGET_IP
+else
+    echo "[!] WhatWeb not found. Please install it with: sudo apt install whatweb"
+fi
+
+echo "[+] Running DNSRecon on $TARGET_IP..."
+if command -v dnsrecon >/dev/null 2>&1; then 
+    dnsrecon -t std -n 8.8.8.8 -d $TARGET_IP
+else 
+    echo "[!] DNSRecon not found please install it with sudo apt install dnsrecon"
+fi
+
+echo "[+] Running Nmap on $TARGET_IP..."
+if command -v nmap >/dev/null 2>&1; then 
+    nmap -sV -Pn $TARGET_IP
+else 
+    echo "[!] Nmap not found. please install it with sudo apt install nmap"
+fi
